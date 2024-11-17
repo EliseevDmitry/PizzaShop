89aa978da54e28b04c7d090e99abc7237f0a7464
Dmitry <dimasel@gmail.com>
2024-11-18 01:37:18 +0700

    Create Project without storyboard

---
+++ PizzaShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaShop
+//
+//  Created by Dmitriy Eliseev on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
